7da78c55a7592732e8d849ecdffbac547e6a5bee
Duy Le <lehongduy.bk@gmail.com>
2022-03-25 19:01:10 +0700

    lambda function

---
+++ app/src/main/java/com/example/lesson12_kotlin/Lesson13.kt
@@ -0,0 +1,25 @@
+package com.example.lesson12_kotlin
+
+import com.example.lesson12_kotlin.model.Student
+
+fun main() {
+    val upperCase1: (String, Int) -> String = { str: String , number : Int -> str.uppercase() + number.toString() } // 1
+
+    val upperCase2: (String) -> String = { str -> str.uppercase() }         // 2
+
+    val upperCase3 = { str: String -> str.uppercase() }                     // 3
+
+     //val upperCase4 = { str : Int -> str.uppercase() }                          // 4
+
+    val upperCase5: (Student) -> String = { it.sName }                 // 5
+
+    val upperCase6: (Student) -> Int = Student::age                  // 6
+
+    println(upperCase1("hello codefresher - lesson", 13))
+    println(upperCase2("hello"))
+    println(upperCase3("hello"))
+
+    var student = Student("Nguyen Van Long", 20, "", false)
+    println(upperCase5(student))
+    println(upperCase6(student))
+}
